42e2cc2be41b9018babce85fba146ba58fa5a817
wangjue <wangjue60@yahoo.com>
2021-01-03 14:43:31 -0500

    35.search insert position

---
+++ Problems/27.RemoveElement/RemoveElement.java
@@ -0,0 +1,12 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int pointer = 0;
+        for(int i =0;i<nums.length; i++){
+            if(nums[i] != val){
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+        return pointer;
+    }
+}
