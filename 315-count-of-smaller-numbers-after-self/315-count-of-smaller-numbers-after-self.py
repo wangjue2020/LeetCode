85495d0554efdc3db00c5f852ad3a6e7942826c8
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-01-13 20:50:52 +0800

    Time: 3660 ms (46.76%), Space: 47.9 MB (11.44%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,63 @@
+class Solution:
+    def countSmaller(self, nums):
+        sort = sorted(nums)
+        # print(len(sort))
+        mid = (sort[-1]+sort[0])//2
+        reversed = self.reverse(nums)
+        ans = []
+        bst = self.buildbst(sort)
+        for cur in reversed:
+            count = 0
+            tmp = bst
+            while True:
+                if tmp.val < cur:
+                    count += tmp.freq+tmp.num_left
+                    if tmp.right == None:
+                        tmp.right = BSTNode(cur)
+                        break
+                    tmp = tmp.right
+                elif tmp.val > cur:
+                    if tmp.left == None:
+                        tmp.left = BSTNode(cur)
+                        tmp.num_left+=1
+                        break
+                    tmp.num_left+=1
+                    tmp = tmp.left
+                else:
+                    tmp.freq+=1
+                    count += tmp.num_left
+                    break
+            ans.append(count)
+        return self.reverse(ans)
+                        
+    def reverse(self, nums):
+        if len(nums) == 1:
+            return nums
+        else:
+            mid = len(nums)//2
+            left = self.reverse(nums[:mid])
+            right = self.reverse(nums[mid:])
+            return right+left
+    
+    def buildbst(self, nums):
+        if nums==[]:
+            return None
+        elif len(nums) == 1:
+            return BSTNode(nums[0])
+        else:
+            mid = len(nums)//2
+            root = BSTNode(nums[mid])
+            root.left = self.buildbst(nums[:mid])
+            if mid+1 < len(nums):
+                root.right = self.buildbst(nums[mid+1:])
+            return root
+    
+        
+    
+class BSTNode:
+    def __init__(self, val):
+        self.val=val
+        self.left=None
+        self.right=None
+        self.freq=0
+        self.num_left=0
