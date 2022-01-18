a4c01103e66655da5985872caf5f6a5f9107da72
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-01-18 22:40:00 +0800

    Time: 322 ms (36.54%), Space: 14.9 MB (57.25%) - LeetHub

--- 707-design-linked-list/707-design-linked-list.py
+++ 707-design-linked-list/707-design-linked-list.py
@@ -1,34 +1,62 @@
+class Node:
+    def __init__(self, val, nxt=None):
+        self.val = val
+        self.next = nxt
 class MyLinkedList:
 
     def __init__(self):
-        self.list = []
+        self.linkedlist = Node(-1)
         self.len = 0
+        self.tail = self.linkedlist
 
     def get(self, index: int) -> int:
         if index > self.len-1:
             return -1
         else:
-            return self.list[index]
+            tmp = self.linkedlist
+            for i in range(index):
+                tmp = tmp.next
+            return tmp.next.val
 
     def addAtHead(self, val: int) -> None:
-        self.list = [val] + self.list
-        self.len += 1
+        backup = self.linkedlist.next
+        cur = Node(val,backup)
+        self.linkedlist.next = cur
+        self.len+=1
+        if self.tail == self.linkedlist:
+            self.tail = cur
 
     def addAtTail(self, val: int) -> None:
-        self.list = self.list+[val]
+        cur = Node(val)
+        self.tail.next = cur
+        self.tail = self.tail.next
         self.len+=1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < self.len:
-            self.list.insert(index,val)
-            self.len+=1
+        if index > self.len:
+            return
         if index == self.len:
             self.addAtTail(val)
+        else:
+            tmp = self.linkedlist
+            for i in range(index):
+                tmp = tmp.next
+            backup = tmp.next
+            cur =  Node(val, backup)
+            tmp.next = cur
+            self.len+=1
+            
 
     def deleteAtIndex(self, index: int) -> None:
         if index <= self.len-1:
-            self.list.pop(index)
+            tmp = self.linkedlist
+            for i in range(index):
+                tmp = tmp.next
+            tmp.next = tmp.next.next
+            if index == self.len-1:
+                self.tail = tmp
             self.len-=1
+            
 
 
 # Your MyLinkedList object will be instantiated and called as such:
