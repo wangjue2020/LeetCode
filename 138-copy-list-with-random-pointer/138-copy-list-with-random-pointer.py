cf813130752e85b5cae6d7d44774414eb74a5ba6
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-02-03 20:40:52 +0800

    Time: 44 ms (50.85%), Space: 15 MB (41.88%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,39 +9,24 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        ans = Node(-1)
-        ptr = ans
-        ori = {}
-        new = []
-        ind = 0
+        d = {}
+        ans = None
+        tmp = ans
         while head != None:
-            if head not in ori:
-                ptr.next = Node(head.val)
-                ori[head] = ind
-                new.append(ptr.next)
-                ind+=1
-            else:
-                ptr.next = new[ori[head]]
-            newnext = None
+            if ans == None:
+                ans = Node(head.val)
+                tmp = ans
+                d[head] = tmp
             if head.next != None:
-                if head.next not in ori:
-                    newnext = Node(head.next.val)
-                    ori[head.next] = ind
-                    new.append(newnext)
-                    ind+=1
-                else:
-                    newnext = new[ori[head.next]]
-            newrandom = None
+                if head.next not in d:
+                    n = Node(head.next.val)
+                    d[head.next] = n
+                tmp.next = d[head.next]
             if head.random != None:
-                if head.random not in ori:
-                    newrandom = Node(head.random.val)
-                    ori[head.random] = ind
-                    new.append(newrandom)
-                    ind+=1
-                else:
-                    newrandom = new[ori[head.random]]
-            ptr.next.next = newnext
-            ptr.next.random = newrandom
-            ptr = ptr.next
-            head= head.next
-        return ans.next
+                if head.random not in d:
+                    r = Node(head.random.val)
+                    d[head.random] = r
+                tmp.random = d[head.random]
+            head = head.next
+            tmp = tmp.next
+        return ans
