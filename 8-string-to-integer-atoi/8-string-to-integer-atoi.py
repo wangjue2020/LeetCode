8bbdecb76dd9e9089e663b4836d5d0332f4ca769
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-04-14 17:28:11 +0800

    Time: 32 ms (96.30%), Space: 13.9 MB (33.42%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,42 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        ans = 0
+        start = False
+        f = False
+        divid = 1
+        positive = True
+        max_int = 2**31-1
+        min_int = -2**31
+        for i in range(len(s)):
+            cur = s[i]
+            if not start and cur == ' ':
+                continue
+            if not start and cur in ['-', '+']:
+                positive = False if cur == '-' else True
+                start = True
+                continue
+            if cur == '.':
+                f = True
+                divid = 10
+                break
+            if cur.isdigit():
+                start = True
+                if not f :
+                    if ans < (max_int-int(cur)) / 10:
+                        ans = ans * 10 + int(cur)
+                    else:
+                        if not positive and ans < (2**31-int(cur)) / 10:
+                            ans = ans * 10 + int(cur)
+                        else:
+                            return max_int if positive else min_int
+                else:
+                    if ans < (max_int - int(cur)/divid) / 10:
+                        ans = ans + int(cur)/divid
+                    else:
+                        if not positive and ans < (2**31-int(cur)/divid) / 10:
+                            ans = ans * 10 + int(cur)/divid
+                        return max_int if positive else min_int
+                    
+            else:
+                break
+        return ans if positive else -ans
