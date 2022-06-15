89aa0950174895f2b346e49a5e908d039aa87631
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-06-15 17:24:18 +0800

    Time: 922 ms (14.11%), Space: 61.1 MB (87.55%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -3,15 +3,11 @@ class RandomizedSet:
 
     def __init__(self):
         self.set = {}
-        self.corpus = ListNode(-1, None)
-        self.last = self.corpus
     def insert(self, val: int) -> bool:
         if val in self.set:
             return False
         self.set[val] = val
-        node = ListNode(val, None)
-        self.last.next = node
-        self.last = node 
+
         return True
     def remove(self, val: int) -> bool:
         if val in self.set:
@@ -20,26 +16,6 @@ class RandomizedSet:
         return False
         
     def getRandom(self) -> int:
-        tmp = self.corpus.next
-        while tmp != None and tmp.val not in self.set:
-            tmp = tmp.next
-        if tmp.next == None:
-            return tmp.val
-        self.corpus.next = tmp.next
 
-        self.last.next = tmp
-        self.last = tmp
-        tmp.next = None
         return choice(list(self.set.keys()))
-    def __str__(self):
-        s = ''
-        tmp = self.corpus
-        while tmp != None:
-            s+= str(tmp.val) + "-->"
-            tmp = tmp.next
-        return s
-        
-class ListNode:
-    def __init__(self, val, next):
-        self.val = val
-        self.next = next
+
