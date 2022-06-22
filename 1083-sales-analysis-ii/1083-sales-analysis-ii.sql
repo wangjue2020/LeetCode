ec3361119190ce49f47e0689563643e1ab39d043
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-06-22 19:25:41 +0800

    Time: 1119 ms (68.12%), Space: 0B (100.00%) - LeetHub

---
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select distinct buyer_id
+from sales join product using(product_id)
+where product_name = 'S8' and buyer_id not in (select buyer_id
+from sales join product using(product_id)
+where product_name = 'iphone' )
