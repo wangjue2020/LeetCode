42269540e9e55e350d917bc0dc4f3a814498cca5
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-06-23 14:31:11 +0800

    Time: 125 ms (22.97%), Space: 13.8 MB (86.06%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,26 +5,28 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        addon = 0
-        ans = l1
-        prev1 = None
-        prev2 = None
-        while l1!= None:
-            if addon == 0 and l2 == None:
-                break
-            if addon != 0:
-                l1.val += addon
-            if l2 != None:
-                l1.val += l2.val
-                l2 = l2.next
-            addon = l1.val // 10
-            l1.val = l1.val % 10
-            prev1 = l1
-            l1 = l1.next
-            if l2 != None and l1==None:
-                prev1.next = l2
-                l1 = l2
-                l2 = None
-        if addon == 1:
-            prev1.next = ListNode(1)
-        return ans
+        add = 0
+        p1 = l1
+        p2 = l2
+        prev = None
+        while p1 != None and p2 != None:
+            add = p1.val + p2.val + add
+            p1.val = add % 10
+            add = add // 10
+            prev = p1
+            p1 = p1.next
+            p2 = p2.next
+        if p2 != None:
+            prev.next = p2
+            p1 = p2
+        if p1 != None:
+            while p1 != None:
+                add =p1.val + add
+                p1.val = add % 10
+                add = add // 10
+                prev = p1
+                p1 = p1.next
+        if add == 1:
+            prev.next = ListNode(1)
+        return l1
+        
