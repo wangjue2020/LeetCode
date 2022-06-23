1ecf965d8a2ba034d9fb2d72c32176b78b6032a2
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-06-23 12:55:44 +0800

    Time: 1020 ms (69.28%), Space: 75.8 MB (48.15%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -3,72 +3,54 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.list = {}
-        self.first = ListNode(-1, None, None, None)
-        self.last = ListNode(-1, None, self.first, None)
+        self.first = ListNode(-1)
+        self.last = ListNode(-1,prev = self.first)
         self.first.next = self.last
         self.size = 0
+    
     def _remove_node(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
-    def _add_node(self, node):
+    def _add_node(self,node):
         node.prev = self.last.prev
         self.last.prev.next = node
-        node.next = self.last 
+        node.next = self.last
         self.last.prev = node
-
-    def _move_to_end(self, node):
-        self._remove_node(node)
-        self._add_node(node)
-
-    def _remove_top(self):
-        top = self.first.next
-        del self.list[top.key]
-        self.first.next = self.first.next.next
-        self.first.next.prev = top.prev
-
+        
     def get(self, key: int) -> int:
-        node = self.list.get(key)
-        if node is None:
+        cur = self.list.get(key)
+        if cur is None:
             return -1
-        self._move_to_end(node)
-        return node.val
+        else:
+            self._remove_node(cur)
+            self._add_node(cur)
+            return cur.val
 
     def put(self, key: int, value: int) -> None:
-        node = self.list.get(key)
-        if node != None:
-            node.val = value
-            self._move_to_end(node)
-            self.list[key] = node
+        cur = self.list.get(key)
+        if cur != None:
+            cur.val = value
+            self._remove_node(cur)
+            self._add_node(cur)
         else:
-            node = ListNode(key, value, self.last, None)
-            self._add_node(node)
-            self.size+=1
-            self.list[key] = node
-            if self.size>self.capacity:
-                self._remove_top()
-                self.size-=1
-    def __str__(self):
-        s = ""
-        tmp = self.first
-        while tmp != None:
-            s += f"[{tmp.key}={tmp.val}] -->"
-            tmp = tmp.next
-        ss = ""
-        tmp = self.last
-        while tmp!= None:
-            ss = f"[{tmp.key}={tmp.val}] -->" + ss 
-            tmp = tmp.prev 
-        s += '\n'
-        s += ss
-        return s
-            
-
-
-
+            cur = ListNode(key, val=value)
+            self._add_node(cur)
+            self.list[key] = cur
+            self.size += 1
+            if self.capacity< self.size:
+                self.size -=1
+                del self.list[self.first.next.key]
+                self._remove_node(self.first.next)
+        
 class ListNode:
-    def __init__(self, key, val, prev, next):
+    def __init__(self, key, val=None, prev=None, next=None):
         self.key = key
         self.val = val
-        self.next = next
         self.prev = prev
+        self.next = next
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
