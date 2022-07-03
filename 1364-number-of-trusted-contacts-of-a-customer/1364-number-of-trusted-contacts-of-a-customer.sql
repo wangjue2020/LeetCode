4189dc87c170c7c092b13105f26bd77a4ab51403
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-07-03 11:13:45 +0800

    Time: 1247 ms (76.07%), Space: 0B (100.00%) - LeetHub

--- 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
+++ 1364-number-of-trusted-contacts-of-a-customer/1364-number-of-trusted-contacts-of-a-customer.sql
@@ -6,7 +6,3 @@ from customers cu left join contacts co on cu.customer_id = co.user_id
 group by cu.customer_id
 ) tmp on i.user_id= tmp.customer_id
 order by invoice_id
-
-# select cu.customer_id, cu.customer_name, count(co.user_id) as contacts_cnt, sum(case when co.contact_email in (select email from customers) then 1 else 0 end) as trusted_contacts_cnt
-# from customers cu left join contacts co on cu.customer_id = co.user_id
-# group by cu.customer_id
