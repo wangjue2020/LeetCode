3260c5cdc295ca0d3b1f7f392ec02bdb6f785ccf
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-07-04 21:16:29 +0800

    Time: 53 ms (40.26%), Space: 13.9 MB (11.01%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -2,11 +2,10 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         addon = 1
         for i in range(len(digits)-1, -1,-1):
-            addon = digits[i] + addon
-            if addon <10:
-                digits[i] = addon
+            cur_sum = digits[i] + addon
+            if cur_sum <10:
+                digits[i] = cur_sum
                 return digits
             else:
-                digits[i] = addon - 10
-                addon = 1
+                digits[i] = cur_sum - 10
         return [1]+digits
