dbfc7ddae8ff8b58e474166e7fbf022ab49256a8
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-07-05 12:41:12 +0800

    Time: 1866 ms (10.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1715-count-apples-and-oranges/1715-count-apples-and-oranges.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select sum(b.apple_count) + sum(ifnull(c.apple_count,0)) as apple_count, sum(b.orange_count) + sum(ifnull(c.orange_count,0)) as orange_count from boxes b left join chests c using(chest_id);
