3dc46c936f6353d3f0144abab29a15669d6cc3a0
wangjue2020 <74479164+wangjue2020@users.noreply.github.com>
2022-07-08 12:20:06 +0800

    Time: 480 ms (99.55%), Space: 0B (100.00%) - LeetHub

---
+++ 2205-the-number-of-users-that-are-eligible-for-discount/2205-the-number-of-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select count(distinct user_id) as user_cnt
+      from purchases
+      where time_stamp between startDate and endDate and amount >= minAmount
+  );
+END
